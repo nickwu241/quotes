5ba097ffe4d2673e059e25e0f4e98dfb361be9cd
nickwu241 <nickwu241@gmail.com>
2018-07-21 23:00:59 -0400

    Create app.py Flask application

---
+++ app.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python
+import json
+import os
+
+from flask import Flask, jsonify
+app = Flask(__name__)
+
+with open('quotes.json') as f:
+    QUOTES = json.load(f)
+
+@app.route('/')
+def index():
+    return 'Hello! GET /quotes for a list of quotes :)'
+
+@app.route('/quotes')
+def quotes():
+    return jsonify(QUOTES)
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
