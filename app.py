194c2e43bae10c8e4acc2a3787a6d3722be88532
nickwu241 <nickwu241@gmail.com>
2018-07-24 23:01:34 -0400

    Add random and get by id

--- app.py
+++ app.py
@@ -1,8 +1,9 @@
 #!/usr/bin/env python
 import json
+import random
 import os
 
-from flask import Flask, jsonify
+from flask import abort, Flask, jsonify
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 
@@ -16,6 +17,16 @@ def index():
 @app.route('/quotes')
 def quotes():
     return jsonify(QUOTES)
+
+@app.route('/quotes/random')
+def quotes_random():
+    return jsonify(random.choice(QUOTES))
+
+@app.route('/quotes/<int:quote_id>')
+def quotes_id(quote_id):
+    if quote_id >= len(QUOTES):
+        abort(404)
+    return jsonify(QUOTES[quote_id])
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 8080)))
